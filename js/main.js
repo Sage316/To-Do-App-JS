ac1ccd9b9951e980577581484644126b2057efaa
Sage316 <115668242+Sage316@users.noreply.github.com>
2023-04-19 21:49:56 +0200

    added sufficient comments and attempted to output saved data from local storage

--- js/main.js
+++ js/main.js
@@ -3,44 +3,58 @@ window.addEventListener('load', () => {
     const input = document.querySelector("#task-input");
     const list = document.querySelector("#tasks");
 
-      
 
-var all = [];
+    // empty array for the new data added
+    var all = [];
 
- // event listener on submit
-        form.addEventListener('submit', (e) => {
+    // event listener on submit
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
-            // empty array for the new data added
-        
+
+
         // assign input value to variable
-       
         const task = input.value;
         if (!task) {
             return;
         }
-        
+
         var a = task.toLowerCase()
         all.push(a)
         console.log(all)
-        
-        console.log(all.sort()); 
 
-      
+        console.log(all.sort());
 
-// Created a div and added a task
+
+
+        // Created a div and added a task
         const task_div = document.createElement("div");
         task_div.classList.add("task");
         list.appendChild(task_div);
+        const test = document.getElementsByClassName('task-list');
+        sessionStorage.setItem("actions", test);
+        let taskaction = sessionStorage.getItem("actions");
 
+        //Attempted to display html from session storage
+        if (taskaction === null) {
+            setBoxColour("colour");
+        } else {
+            setBoxColour(taskaction);
+        }
 
+        function setBoxColour(personName) {
+            const example = document.getElementsByClassName("task-list");
+            example.value = personName;
+            example.classList.appendChild(example);
+        }
 
+        //Creatig a new div to contain the user output (the new todo input) 
         const task_content_div = document.createElement("div");
         task_content_div.classList.add("content");
         task_div.appendChild(task_content_div);
 
-        
 
 
+        // Creating a input for the users entered value that is editable
         const task_input = document.createElement("input");
         task_input.classList.add("text");
         task_input.type = "text";
@@ -50,19 +64,21 @@ var all = [];
 
         // added todo items to session storage
         sessionStorage.setItem("items", task);
-        let personName = sessionStorage.getItem("lastname");  
+        let personName = sessionStorage.getItem("lastname");
         document.getElementsByClassName("text").innerHTML = personName;
 
-
+        // created a div that cotains the action buttons
         const task_actions_div = document.createElement("div");
         task_actions_div.classList.add("actions");
         task_div.appendChild(task_actions_div);
+
+
 
         //Edit button feature added
         const task_edit_botton = document.createElement("button");
         task_edit_botton.classList.add("Edit");
         task_edit_botton.innerHTML = "Edit";
-        
+
         //Delete button function added
         const task_delete_button = document.createElement("button");
         task_delete_button.classList.add("Delete");
@@ -73,12 +89,12 @@ var all = [];
         task_completed_button.classList.add("Completed");
         task_completed_button.innerHTML = "Completed";
 
-
+        // Appending the action buttons to html
         task_actions_div.appendChild(task_edit_botton);
         task_actions_div.appendChild(task_completed_button);
         task_actions_div.appendChild(task_delete_button);
 
-       
+        // added a event-listner that listens for a button click in order to perform the edit and save funtionality  
         task_edit_botton.addEventListener('click', () => {
 
             if (task_edit_botton.innerText.toLowerCase() == "edit") {
@@ -93,7 +109,7 @@ var all = [];
             }
         });
 
-         //Delete pop up created
+        //Delete pop up created
         task_delete_button.addEventListener('click', () => {
             if (confirm("Are you sure you want to delete this task?")) {
                 list.removeChild(task_div);
@@ -102,7 +118,9 @@ var all = [];
             }
         })
 
-        // made line-through text decoration visible
+        // made line-through text decoration visible         
+        // added a event-listner that listens for a button click in order to perform the edit and save funtionality  
+
         task_completed_button.addEventListener('click', () => {
             task_input.style.textDecoration = "line-through";
             task_input.style.textDecorationColor = "red";
